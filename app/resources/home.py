80f796584346bd62eb9e699bfc5135b2daba267e
qq774525000 <774525000@qq.com>
2019-10-17 17:01:22 +0800

    many pages

--- app/resources/home.py
+++ app/resources/home.py
@@ -1,6 +1,11 @@
 from flask_restful import Resource
+from flask import request
 
 
 class Get_List(Resource):
 	def get(self):
 		return { 'name': 'shuaige'}
+
+	def post(self):
+		print(request.form['name'])
+		return {'age': 18}
